4a3bd4b1ab05ab0d10294776d6d374e199cdacf0
Maxime Lechat-Tissier <maxime.lechat.tissier@gmail.com>
2020-02-28 13:42:59 +0100

    Added edior config

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const assert = require('assert');
 const crypto = require('crypto');
 const fs = require('fs');
 
-const yaml = require('yaml');
 const { BadRequest, NotFound, Unauthorized } = require('http-errors');
 const bodyParser = require('body-parser');
+const log = require('loglevel');
+const yaml = require('yaml');
 
 const { PORT, CONFIG } = process.env;
 
@@ -26,19 +27,20 @@ for (const conf in config.repositories) {
 function verify(req, res, buf, encoding) {
   const { repoId } = req.params;
   const conf = config.repositories[repoId];
+
   if (!conf) {
     throw new NotFound('Repository not found');
   }
 
   if (!req.get('X-Hub-Signature')) {
-    console.warn('Missing signature header');
+    log.warn('Missing signature header');
     throw new BadRequest('Missing signature header');
   }
 
   const sig = crypto.createHmac('sha1', conf.secret).update(buf).digest('hex');
 
   if (req.get('X-Hub-Signature') !== `sha1=${sig}`) {
-    console.warn('Invalid signature', req.get('X-Hub-Signature'), 'expected: ', sig);
+    log.warn('Invalid signature', req.get('X-Hub-Signature'), 'expected: ', sig);
     throw new Unauthorized('Invalid signature header');
   }
 
@@ -58,18 +60,18 @@ function exec(script) {
   const taskId = Math.random().toString(16).slice(-8);
   const task = spawn('sh', script.split(' ').filter(e => !!e.trim()));
 
-  console.log(`Starting task ${taskId} on ${new Date()}`);
+  log.info(`Starting task ${taskId} on ${new Date()}`);
 
   task.stdout.on('data', (data) => {
-    console.log(format(taskId, 'stdout', data));
+    log.info(format(taskId, 'stdout', data));
   });
 
   task.stderr.on('data', (data) => {
-    console.log(format(taskId, 'stderr', data));
+    log.info(format(taskId, 'stderr', data));
   });
 
   task.on('close', (code) => {
-    console.log(`Task ${taskId}: child process exited with code ${code}`);
+    log.info(`Task ${taskId}: child process exited with code ${code}`);
   });
 }
 
@@ -81,10 +83,12 @@ app.post('/github-hook/:repoId', bodyParser.json({ verify }), (req, res, next) =
   const commit = body.head_commit;
   const repository = body.repository;
 
-  if (req.get('x-github-event') !== 'push')
-    return res.send('osef');
+  if (req.get('x-github-event') !== 'push') {
+    res.send('osef');
+    return;
+  }
 
-  console.log(`Received hook for repository: "${repository.full_name} (repoId=${repoId})", commit "${commit.message}" done by "${commit.author.username}" (commit id=${commit.id})`);
+  log.info(`Received hook for repository: "${repository.full_name} (repoId=${repoId})", commit "${commit.message}" done by "${commit.author.username}" (commit id=${commit.id})`);
 
   exec(conf.script);
 
@@ -101,5 +105,5 @@ app.use((error, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`github-hook listening on port ${PORT}!`);
+  log.info(`github-hook listening on port ${PORT}!`);
 });
